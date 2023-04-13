c71dc6040a49cc44623b65fa6c59a7f06b35faa9
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-04-13 14:17:16 +0100

    Added search / box and event listener for button

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// Append list items with button
+// Append item to
 
+var submitButton = document.getElementById("submit-item");
+
+submitButton.addEventListener("click", appendListItem);
+
+
+
+function appendListItem() {
+    console.log("Item appended!")
+    // Append to list-doing, list-done, list-todo
+
+}
